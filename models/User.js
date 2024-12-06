7ca838270e25a50ec1291bd63de92193811aba54
Macbook pro <macbookpro@YourexBoy.local>
2024-12-06 08:50:44 -0800

    G

--- models/User.js
+++ models/User.js
@@ -2,17 +2,12 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   
-  firstName: {
+  fullName: {
     type: String,
     required: true,
   },
 
-  lastName: {
-    type: String,
-    required: true,
-  },
-
-  userName: {
+  gender: {
     type: String,
     required: true,
   },
@@ -34,6 +29,11 @@ const UserSchema = new mongoose.Schema({
   },
 
   country: {
+    type: String,
+    required: true,
+  },
+  
+  address: {
     type: String,
     required: true,
   },
